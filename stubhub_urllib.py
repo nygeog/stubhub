0cdd1e07049829eea4e1f916cf6f08f39a41d993
Danny S. - MacMini <DanielMSheehan@Mac-mini.local>
2015-10-03 09:35:37 -0400

    update

--- stubhub_urllib.py
+++ stubhub_urllib.py
@@ -2,14 +2,16 @@ import urllib
 import time
 
 pauser = 60 #1 minute for testing
-pauser = 4000 #almost an hour
+pauser = 3600 #an hour
 
 eventList = ['9394538','9394539','9394540','9394541']
 dateList  = ['20151230','20151231','20160101','20160102']
 
-for i, j in zip(eventList, dateList):
-	theTime = time.strftime("%Y%m%d%H%M")
-	print theTime
-	urllib.urlretrieve ('http://www.stubhub.com/ticketAPI/restSvc/event/'+i+'.json', 'data/ph'+j+'_'+i+'_'+theTime+'.json')
 
-	time.sleep(pauser)
+for i in range(1,3001):
+	for i, j in zip(eventList, dateList):
+		theTime = time.strftime("%Y%m%d%H%M")
+		print theTime
+		urllib.urlretrieve ('http://www.stubhub.com/ticketAPI/restSvc/event/'+i+'.json', 'data/ph'+j+'_'+i+'_'+theTime+'.json')
+
+		time.sleep(pauser)
