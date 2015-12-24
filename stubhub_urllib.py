ca40cc046b064d7dff7e617160592e1811e1e1fc
Danny S. - MacMini <DanielMSheehan@Mac-mini.local>
2015-12-24 11:34:45 -0500

    update

--- stubhub_urllib.py
+++ stubhub_urllib.py
@@ -1,8 +1,9 @@
 import urllib
 import time
 
-pauser = 60 #1 minute for testing
+pauser =   60 #1 minute for testing
 pauser = 3600 #an hour
+pauser =  450 #7.5 minutes, grab each every half hour
 
 eventList = ['9394538','9394539','9394540','9394541']
 dateList  = ['20151230','20151231','20160101','20160102']
