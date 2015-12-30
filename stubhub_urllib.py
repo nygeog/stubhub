b76eca8c649241970b58049444a402e3a897396b
Danny S. - MacMini <DanielMSheehan@Mac-mini.local>
2015-12-30 14:58:46 -0500

    update

--- stubhub_urllib.py
+++ stubhub_urllib.py
@@ -11,8 +11,13 @@ dateList  = ['20151230','20151231','20160101','20160102']
 
 for i in range(1,3001):
 	for i, j in zip(eventList, dateList):
-		theTime = time.strftime("%Y%m%d%H%M")
-		print theTime
-		urllib.urlretrieve ('http://www.stubhub.com/ticketAPI/restSvc/event/'+i+'.json', 'data/ph'+j+'_'+i+'_'+theTime+'.json')
-
-		time.sleep(pauser)
+		try:
+			theTime = time.strftime("%Y%m%d%H%M")
+			print 'event: ', i, j
+			print theTime
+			urllib.urlretrieve ('http://www.stubhub.com/ticketAPI/restSvc/event/'+i+'.json', 'data/ph'+j+'_'+i+'_'+theTime+'.json')
+			time.sleep(pauser)
+		except:
+			print 'event: ', i, j, 'did not work'
+			#pass
+		
